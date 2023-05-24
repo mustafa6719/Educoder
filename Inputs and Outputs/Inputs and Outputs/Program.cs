e36a501c8aecf541290276a4286dfe997cfa2c6a
Educoder <mustafa6719@hotmail.co.uk>
2023-05-24 22:47:07 +0100

    initial

--- Inputs and Outputs/Inputs and Outputs/Program.cs
+++ Inputs and Outputs/Inputs and Outputs/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to the Input/Output program!");
+
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        Console.Write("Please enter your age: ");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Thank you for providing your information.");
+        Console.WriteLine("Your name is: " + name);
+        Console.WriteLine("Your age is: " + age);
+
+        Console.WriteLine("Press any key to exit.");
+        Console.ReadKey();
+    }
+}
